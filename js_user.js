ba683472575f75418fef71e59fffd876543a96be
ElZiga <krutt0610@gmail.com>
2021-12-18 18:58:59 +0200

    zxc

--- js_user.js
+++ js_user.js
@@ -3,8 +3,10 @@ URLSearchParams(window.location.search);
 
 const id = urlParams.get('id');
 
+
 let profile = 
 document.getElementById('profile');
+
 
 let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
 
